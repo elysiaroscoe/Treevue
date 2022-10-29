194855b6f99fda111136c8da014285533d3c78f3
E Roscoe <elysiaroscoe@gmail.com>
2022-10-29 15:18:04 -0700

    homepage components

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './Header'
+import {Footer} from "./Footer"
+import {Main} from "./Main"
+
+
+export const Landing = () => {
+  return (
+    <div>
+        <Header/>
+        <Main />
+        <Footer />
+    </div>
+  )
+}
