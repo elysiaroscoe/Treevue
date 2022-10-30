b5c39a261201913b0eca889893c96c4615515e2e
Steven Wing <steven.wing82@gmail.com>
2022-10-30 11:49:40 -0700

    add basic styling

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,19 +8,35 @@ export const Info = () => {
     <>
     <Typography>Overall Impact of Tree Canopy Score</Typography>
     <Section>
-    <Typography>Air Quality</Typography>
+      <Icon>image</Icon>
+      <Typography>Air Quality</Typography>
+      <InfoSection>test</InfoSection>
     </Section>
     <Section>
-    <Typography>Safety</Typography>
+      <Icon></Icon>
+      <Typography>Safety</Typography>
+      <InfoSection></InfoSection>
     </Section>
     <Section>
-    <Typography>Temperature</Typography>
+      <Icon></Icon>
+      <Typography>Temperature</Typography>
+      <InfoSection></InfoSection>
     </Section>
     </>
   )
 }
+
+// Styling
 const Section = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-around;
+`
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
   justify-content: center;
+`
+const InfoSection = styled.p`
+  width: 70%;
 `
