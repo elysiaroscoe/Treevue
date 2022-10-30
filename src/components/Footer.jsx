e337421de747d50a48674978431e5211d8623342
E Roscoe <elysiaroscoe@gmail.com>
2022-10-30 11:49:17 -0700

    credits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
+      <Typography>Collaborators: Theresa Benkman, Khong Lor, E. Roscoe, Teressa Sung, Steven Wing</Typography>
+      <Typography>Created at HackSEA 2022 with <Link href={"https://www.hackathonsinternational.com/"}>Hackathons International</Link></Typography>
       <Link href={"https://railway.app/"}>Thank you to Railway for hosting this project</Link>
-      <Typography>Collaborators: Theresa Benkman, Khong Lor, E. Roscoe, Teressa Sung, Steven Wing </Typography>
     </>
   )
 }
